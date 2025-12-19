8081d32c3c0207a1039a34492c95cf85b3e96e79
github-ci <you@example.com>
2025-12-19 17:23:41 +0800

    upgrade

--- src/DependencyInjection/WechatOfficialAccountFansExtension.php
+++ src/DependencyInjection/WechatOfficialAccountFansExtension.php
@@ -4,7 +4,7 @@ namespace Tourze\WechatOfficialAccountFansBundle\DependencyInjection;
 
 use Tourze\SymfonyDependencyServiceLoader\AutoExtension;
 
-class WechatOfficialAccountFansExtension extends AutoExtension
+final class WechatOfficialAccountFansExtension extends AutoExtension
 {
     protected function getConfigDir(): string
     {
